bdbb1b2f06b6054e45140e2e2aff5e58b90351f2
vsvood <kaspirovich.vm@phystech.edu>
2021-09-23 01:14:20 +0300

    deleting unnecessary files

--- custom_char_types.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by vsvood on 9/19/21.
-//
-
-#ifndef TEXTHOLDER_CUSTOM_CHAR_TYPES_H
-#define TEXTHOLDER_CUSTOM_CHAR_TYPES_H
-
-#endif  // TEXTHOLDER_CUSTOM_CHAR_TYPES_H
--- custom_lines_lib.cpp
+++
@@ -1,7 +0,0 @@
-//
-// Created by vsvood on 9/19/21.
-//
-
-#include "custom_lines_lib.h"
-
-#include "custom_char_types.h"
